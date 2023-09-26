2044ea65fcd85f86ee36c5cf390f14585107f008
Manu Srimat <msrimat@gmail.com>
2023-09-26 11:50:15 -0700

    starting to add firestore db to handle user input

--- src/components/FormContent.tsx
+++ src/components/FormContent.tsx
@@ -1,23 +1,32 @@
-import React, { FormEvent } from "react";
-import { useForm } from "react-hook-form";
+import React, { FormEvent, useEffect, useState } from "react";
 import "./FormContent.css"; // Import your custom CSS styles for the component
+import "../db/db.ts";
+import { collection, onSnapshot } from "firebase/firestore";
+import db from "../db/db.ts";
+import { FormEventHandler } from "react";
 
 const FormContent: React.FC = () => {
-  const { register } = useForm();
+  const [posterName, setPosterName] = useState("");
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    const data = new FormData(event.target as HTMLFormElement);
-    const poster = data.get("poster");
-    console.log(poster);
+    setPosterName((event.currentTarget[0] as HTMLInputElement).value);
+    console.log(posterName);
   };
+
+  useEffect(() => {
+    onSnapshot(collection(db, "imgs"), (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+      });
+    });
+  }, []);
 
   return (
     <form onSubmit={handleSubmit} className="main-content-container">
       <h1 className="main-content-title">Generate a Poster</h1>
       <div className="input-container">
         <input
-          {...register("poster")}
           id="name"
           type="text"
           className="form-control"
@@ -29,6 +38,29 @@ const FormContent: React.FC = () => {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+      </div>
+      <div>
+        <h1 className="text-3xl pt-8">These are your posters!</h1>
+        <div className="row">
+          <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+            <img
+              src="https://cdn.britannica.com/85/235885-050-C8CC6D8B/Samoyed-dog-standing-snow.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+            />
+          </div>
+          <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+            <img
+              src="https://cdn.britannica.com/85/235885-050-C8CC6D8B/Samoyed-dog-standing-snow.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+            />
+          </div>
+          <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+            <img
+              src="https://cdn.britannica.com/85/235885-050-C8CC6D8B/Samoyed-dog-standing-snow.jpg"
+              className="w-100 shadow-1-strong rounded mb-4"
+            />
+          </div>
+        </div>
       </div>
     </form>
   );
